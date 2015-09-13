c2b0ed196e87c45073dbf414bda1958660597aac
alexnm <alex.n.moldovan@gmail.com>
2015-09-13 23:34:21 +0300

    initial commit

---
+++ config.rb
@@ -0,0 +1,10 @@
+require 'compass/import-once/activate'
+
+Encoding.default_external = 'utf-8'
+
+http_path = "/"
+css_dir = "scss"
+sass_dir = "scss"
+
+output_style = :uncompressed
+line_comments = false
